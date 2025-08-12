2c1c469aafb06f1c2c63a9cb05a4621124a10f82
Kunal Asude <kunalasude@gmail.com>
2025-08-12 23:29:37 +0530

    FBV and CBV CRUD

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,9 @@ from django.urls import path ,include
 from . import views
 
 urlpatterns = [
-    path("students/",views.studentsView)
+    path("students/",views.studentsView),
+    path("students/<int:pk>/",views.studentDetailView),
+
+    path('employees/',views.employees.as_view()),
+    path('employees/<int:pk>/',views.employeeDetailView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,90 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from students.models import Student
-from .serializers import StudentSerializer
+from employees.models import Employee
+from .serializers import StudentSerializer, EmployeeSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from django.http import Http404
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def studentsView(req):
-    if req.method=='GET':
-        students= Student.objects.all()
-        serializer=StudentSerializer(students,many=True)
+    if req.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif req.method == 'POST':
+        serializer = StudentSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def studentDetailView(req, pk):
+    student = Student.objects.get(pk=pk)
+    if req.method == 'GET':
+        try:
+            serializer = StudentSerializer(student)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    elif req.method == 'PUT':
+        serializer = StudentSerializer(student, data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif req.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class employees(APIView):
+    def get(self, req):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, req):
+        serializer = EmployeeSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class employeeDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            employee = Employee.objects.get(pk=pk)
+            return employee
+        except Employee.DoesNotExist:
+            raise Http404
+        
+    def get(self,req,pk):
+        employee = self.get_object(pk)
+        serializer=EmployeeSerializer(employee)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def put(self,req,pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee,data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else :
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,req,pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
     
-    return Response(serializer.data,status=status.HTTP_200_OK)
